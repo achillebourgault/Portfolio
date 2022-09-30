4818652c9cbb7c48cb4ff502860ac61845720bdd
Achille Bourgault <achille.bourgault@epitech.eu>
2022-09-30 19:29:15 +0100

    [+] Hero Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
 
-export default App;
+  render() {
+    return (
+        <div className={"window"}>
+            <div className={"content"}>
+                <h1>Achille Bourgault</h1>
+                <p>Developpeur - Etudiant à <span>Epitech Lille</span></p>
+                <div className={"links-section"}>
+                    <div className={"links"}><span>Linkedin</span></div>
+                    <div className={"links"}><span>Github</span></div>
+                    <div className={"links"}><span>Contact</span></div>
+                </div>
+            </div>
+
+            <svg className={"svg"} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#156ec6" fill-opacity="1" d="M0,0L80,37.3C160,75,320,149,480,176C640,203,800,181,960,176C1120,171,1280,181,1360,186.7L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+        </div>
+    )
+  }
+}
